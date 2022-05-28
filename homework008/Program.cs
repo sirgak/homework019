5f58b092e62d07d1324929758aae4a66eb0430ab
sirgak <life_4_ever@mail.ru>
2022-05-28 21:39:23 +0300

    test
    test

---
+++ homework008/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace homework008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "exit";
+            string userInput;
+            int trying = 1;
+            while ( trying>0 )
+            {
+                Console.Write("password: ");
+                userInput = Console.ReadLine();
+                if (password == userInput)
+                {
+                    Console.WriteLine("woohoo");
+                    break;
+                }
+                else { }
+            }
+        }       
+    }
+}
